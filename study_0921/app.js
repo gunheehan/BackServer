9cf9f66ebfd56712b57c6cc6e674b99d4437c170
HanGeonHee <han.gh@salin.co.kr>
2025-09-22 18:35:34 +0900

    ui, js 상호작용 예제

--- study_0921/app.js
+++ study_0921/app.js
@@ -1,5 +1,4 @@
-function sayHello(name){
-    console.log("Hello" + name)
-}
-
-sayHello("df")
+const userMsg = prompt();
+const userNumber = parseInt(userMsg);
+const result = userNumber % 2;
+document.querySelector('h1').textContent = result ? "홀수입니다." : "짝수입니다.";
