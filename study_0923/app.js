e3d784cc455fd742ca0b519667eee8ce4bf256b9
HanGeonHee <han.gh@salin.co.kr>
2025-09-23 16:49:35 +0900

    module 실습

---
+++ study_0923/app.js
@@ -0,0 +1,14 @@
+// setInterval(()=>{
+//     console.log('Node js Invoke');
+// }, 3000);
+
+// setTimeout(()=>{
+//     console.log('Node js Timeout');
+// }, 2000);
+
+// Module
+
+// import Module
+const logger = require('./logger.js') // import
+logger.showLogMessage('message Show')
+
---
+++ study_0923/logger.js
@@ -0,0 +1,8 @@
+
+function showLogMessage(message){
+    console.log('======================================');
+    console.log(message);
+    console.log('======================================');
+}
+
+module.exports.showLogMessage = showLogMessage;
