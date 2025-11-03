32e4fa46f966f566b1f93de007eefdc7869a5a8a
GEONHEEHAN <gksrjsgml13@naver.com>
2025-11-04 00:22:22 +0900

    hover virtual study

---
+++ Publishing/Jquery/jquery.js
@@ -0,0 +1,7 @@
+$('p').css({'display':'none'});
+$('show-btn').click(function(){
+    $('p').css({'display':'block'});
+});
+$('show-btn').click(function(){
+    $('p').css({'display':'none'});
+});
